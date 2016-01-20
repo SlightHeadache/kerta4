4f72e69fd01873db8572757b14548e75219aaba1
Juuso Minkkilä <K1756@student.jamk.fi>
2016-01-20 15:51:13 +0200

    I made a radio.

---
+++ 4-5/Program.cs
@@ -0,0 +1,110 @@
+﻿/*
+ * Created: 19/01/2016
+ * Author: Juuso Minkkilä
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Radio radio = new Radio();
+            radio.IsOn = false;
+            radio.Vol = 3;
+            radio.Freq = 5000.3;
+            int choise, volBuffer;
+            double freqBuffer;
+            bool checker;
+            Console.WriteLine("You are looking at a radio. It appears to be off.");
+            for (;;)
+            {
+                if (radio.IsOn == false)
+                {
+                    Console.WriteLine("0. Leave");
+                    Console.WriteLine("1. Press the power switch");
+                    Console.Write(">> ");
+                    checker = int.TryParse(Console.ReadLine(), out choise);
+                    if (checker == false)
+                    {
+                        Console.WriteLine("That's not a valid input.");
+                    }
+                    else if (choise == 0)
+                        break;
+                    else
+                    {
+                        switch (choise)
+                        {
+                            case 1:
+                                Console.WriteLine("You turn the radio on. Its volume is {0} and frequency {1}.", radio.Vol, radio.Freq);
+                                radio.IsOn = true;
+                                break;
+                            default:
+                                Console.WriteLine("That's not a valid input.");
+                                break;
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("0. Leave");
+                    Console.WriteLine("1. Press the power switch");
+                    Console.WriteLine("2. Change volume");
+                    Console.WriteLine("3. Change frequency");
+                    Console.Write(">> ");
+                    checker = int.TryParse(Console.ReadLine(), out choise);
+                    if (checker == false)
+                    {
+                        Console.WriteLine("That's not a valid input.");
+                    }
+                    else if (choise == 0)
+                        break;
+                    else
+                    {
+                        switch (choise)
+                        {
+                            case 1:
+                                Console.WriteLine("You turn the radio off.");
+                                radio.IsOn = false;
+                                break;
+                            case 2:
+                                Console.WriteLine("Give new volume");
+                                Console.Write(">> ");
+                                checker = int.TryParse(Console.ReadLine(), out volBuffer);
+                                if (checker == false)
+                                {
+                                    Console.WriteLine("That's not a valid input.");
+                                }
+                                else
+                                {
+                                    radio.Vol = volBuffer;
+                                    Console.WriteLine("The current volume is {0} and frequency is {1}.", radio.Vol, radio.Freq);
+                                }
+                                break;
+                            case 3:
+                                Console.WriteLine("Give new volume");
+                                Console.Write(">> ");
+                                checker = double.TryParse(Console.ReadLine(), out freqBuffer);
+                                if (checker == false)
+                                {
+                                    Console.WriteLine("That's not a valid input.");
+                                }
+                                else
+                                    radio.Freq = freqBuffer;
+                                Console.WriteLine("The current volume is {0} and frequency is {1}.", radio.Vol, radio.Freq);
+                                break;
+                            default:
+                                Console.WriteLine("That's not a valid input.");
+                                break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ 4-5/Radio.cs
@@ -0,0 +1,79 @@
+﻿/*
+ * Created: 19/01/2016
+ * Author: Juuso Minkkilä
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_5
+{
+    class Radio
+    {
+        //min and max frequencies
+        private readonly double minFreq = 2000.0, maxFreq = 26000.0;
+        //min and max volume
+        private readonly int minVol = 0, maxVol = 9;
+
+        //member fields
+        private double freq;
+        private int vol;
+
+        //properties
+        public double Freq
+        {
+            get
+            {
+                return freq;
+            }
+            set
+            {
+                if (value < minFreq)
+                {
+                    Console.WriteLine("You can't set the frequency that low. Instead, you set it to {0}.", minFreq);
+                    freq = minFreq;
+                }
+                if (value > maxFreq)
+                {
+                    Console.WriteLine("You can't set the frequency that high. Instead, you set it to {0}.", maxFreq);
+                    freq = maxFreq;
+                }
+                else
+                    freq = value;
+            }
+        }
+        public int Vol
+        {
+            get
+            {
+                return vol;
+            }
+            set
+            {
+                if (value < minVol)
+                {
+                    Console.WriteLine("You can't set the volume that low. Instead, you set it to {0}.", minVol);
+                    vol = minVol;
+                }
+                if (value > maxVol)
+                {
+                    Console.WriteLine("You can't set the volume that high. Instead, you set it to {0}.", maxVol);
+                    vol = maxVol;
+                }
+                else
+                    vol = value;
+            }
+        }
+        public bool IsOn { get; set; }
+
+        //construct
+        //public Radio()
+        //{
+        //    IsOn = false;
+        //    Vol = 3;
+        //    Freq = 5000.0;
+        //}
+    }
+}
